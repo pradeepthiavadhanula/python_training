a3a17c51f211dcf1db3812a19a2b0b688e35fbd4
Your Name <avadhanulapradeepthi@gmail.com>
2018-11-23 16:29:32 +0530

    sample test data2

---
+++ eor.py
@@ -0,0 +1,8 @@
+class eor:
+    def input_value(self, num):
+        if (int(num) % 2) == 0:
+            print "Number is Even"
+        else:
+            print "Number is odd"
+           
+                   
